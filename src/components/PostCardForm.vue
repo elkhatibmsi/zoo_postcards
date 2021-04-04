c8ad9a5d0b3d883cfba046b111117f1da1bf404c
Duncan Ferency-Viars <77799251+DuncanFV@users.noreply.github.com>
2021-04-04 12:36:36 -0400

    "Edited PostCardForm to have an entry field (Not sure if it works yet)"

--- src/components/PostCardForm.vue
+++ src/components/PostCardForm.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="postcard-form">
-  <h2>This is where the form will live</h2>
-   </div>
+    <h2>This is where the form will live</h2>
+    <textarea class="postcard-text" v-model="msg">Enter Your Text:</textarea>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'PostCardForm',
   props: {
-    msg: String
+    msg: {
+      type: 'text',
+      value: 'Enter message',
+      required: true
+    }
   }
 }
 </script>
@@ -17,3 +22,25 @@ export default {
 <style scoped>
 
 </style>
+
+
+<!--
+Adapt this method:
+
+onSubmit() {
+          if (this.name === '' || this.review === '' || this.rating === null) {
+              alert('Review is incomplete. Please fill out every field.')
+              return
+          }
+          let productReview = {
+              name: this.name,
+              review: this.review,
+              rating: this.rating
+          }
+          this.$emit('review-submitted', productReview)
+          
+          this.name = ''
+          this.review = ''
+          this.rating = null
+      }
+-->
